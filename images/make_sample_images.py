5036eff3393c247aa44a0556b8262888604df865
slippy0 <dron@alum.mit.edu>
2017-10-19 02:20:18 -0400

    Got make_sample_images working

--- images/make_sample_images.py
+++ images/make_sample_images.py
@@ -3,38 +3,87 @@
 #####################################################
 import numpy as np
 import cv2
-
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
 
 # Image of vertical stripes
 h,w = (1000,1000)
-n_stripes=10
+stripe_w = 20
 
 stripes = np.zeros((h,w))
 
-mask = np.int_(np.arange(w) / n_stripes) % 2
+mask = np.int_(np.arange(w) / stripe_w) % 2
 idx = np.where(mask)
 stripes[:, idx] = 255
 
-
 cv2.imwrite('stripes_input.png', stripes)
 
-X,Y = np.meshgrid(range(w),range(h))
-pts = np.vstack((X.reshape(1,-1), Y.reshape(1,-1)))
-
-pts = pts.astype('float32')
-pts = pts.reshape([1, h*w, 2])
-
-# distort points
+# Distortion Params
+x_0 = w//2
+y_0 = h//2
 K = np.eye(3)
-d = np.zeros(4)
+K[0,0] = x_0
+K[1,1] = y_0
+K[0,2] = x_0
+K[1,2] = y_0
+d = np.array([0., 0., 0., 0.])
 
-distorted_pts = cv2.fisheye.distortPoints(pts, K, d)
+# This does some magic to have the output be a good size
+Knew, valid_roi = cv2.getOptimalNewCameraMatrix(K, d, (w,h), 1)
+K_inv = np.linalg.inv(K)
 
-x_dist = distorted_pts[0,:,0]
-y_dist = distorted_pts[0,:,1]
-distorted_im = cv2.remap(stripes, x_dist, y_dist, interpolation=cv2.INTER_LINEAR)
+# Get pixel coordinates
+X,Y = np.meshgrid(range(w),range(h))
 
-cv2.imwrite('stripes.png', distorted_im)
+# Get ideal image coordinates my multiplying by K_inv
+pts = np.vstack((X.flatten(), Y.flatten(), np.ones(h*w)))
+pts_c = np.dot(K_inv, pts)
+x_c = pts_c[0,:]
+y_c = pts_c[1,:]
 
-print distorted
-print distorted.shape
+# Compute distortion amount
+r = np.sqrt(x_c**2 + y_c**2)
+th = np.arctan(r)
+
+th_2 = th*th
+th_3 = th_2*th
+th_5 = th_3*th_2
+th_7 = th_5*th_2
+th_9 = th_7*th_2
+th_d = th + d[0]*th_3 + d[1]*th_5 + d[2]*th_7 + d[3]*th_9
+
+# These apply undistortion
+#scale = r
+#scale[np.abs(scale) < 1e-5] = 1
+#scale = th_d / scale
+
+# These apply distortion
+scale = th_d
+scale[np.abs(scale) < 1e-5] = 1
+scale = r / scale
+
+# Reshape distortion scalar
+scale = scale.reshape(h,w)
+
+# This is equivilant to K_new * diag(scale,scale,1) * K_inv * [x,y,1]
+x_p = Knew[0,0] / K[0,0] * (X - x_0) * scale + Knew[0,2]
+y_p = Knew[1,1] / K[1,1] * (Y - y_0) * scale + Knew[1,2]
+
+# Convert to the right dtype for remap
+m1 = x_p.astype(np.float32)
+m2 = y_p.astype(np.float32)
+
+
+distorted_im = cv2.remap(stripes, 
+                         x_p.astype(np.float32), 
+                         y_p.astype(np.float32), 
+                         interpolation=cv2.INTER_LINEAR)
+
+cv2.imwrite('stripes_distorted.png', distorted_im)
+
+# Some debugging plots
+#plt.scatter(x_p, y_p, marker='.')
+#plt.axis('equal')
+#plt.show()
+#plt.imshow(distorted_im)
+#plt.axis('equal')
