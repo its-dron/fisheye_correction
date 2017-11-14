7d72635b8620890a6922667497f176d98e44a51d
Brian Pugh <brianpugh@Brians-MacBook-Pro.local>
2017-11-14 16:53:50 -0500

    Very incomplete outline of objective function and how to use minimize

--- edgel.py
+++ edgel.py
@@ -17,7 +17,17 @@ INPUT_IM_PATH = os.path.join(IMAGES_DIR, 'stripes_distorted.png')
 EDGEY_RATIO = 2
 N_THETA = 180
 
-def objective_fn(h_1d):
+def objective_fn(vars_to_optimize, im_edge):
+    '''
+    To be called:
+        scipy.optimize.minimize(objective_fn, initial_guess,  args=(im_edge), method='Nelder-Mead')
+    '''
+
+    # 1. Undistort im_edge by vars_to_optimize
+    # 2. Compute cost of newly undistorted
+    return cost(est_h_1d)
+
+def cost(h_1d):
     '''
     Compute the 1D hough entropy
     '''
