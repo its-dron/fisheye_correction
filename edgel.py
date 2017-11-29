3a469cb98c4b16718bf5f0b2ef248e91d7f8d12a
Daniel Ron <dron@alum.mit.edu>
2017-11-29 12:51:06 -0500

    Documentation and function name cleanup

--- edgel.py
+++ edgel.py
@@ -20,45 +20,59 @@ INPUT_IM_PATH = os.path.join(IMAGES_DIR, 'stripes_distorted.png')
 EDGEY_RATIO = 2
 N_THETA = 180
 
+EPS = 1e-6
+
+DEBUG = True
+
 def objective_fn(vars_to_optimize, im_edge):
-    '''
+    """
     To be called:
         scipy.optimize.minimize(objective_fn, initial_guess,  args=(im_edge), method='Nelder-Mead')
-    '''
+    """
 
     # 1. Undistort im_edge by vars_to_optimize
     # 2. Compute cost of newly undistorted
-    return cost(est_h_1d)
+    #return cost(est_h_1d)
+    pass
 
 def cost(h_1d):
-    '''
-    Compute the 1D hough entropy
-    '''
+    """Compute the 1D hough entropy. Helper for hough_entropy"""
     h_1d /= np.sum(h_1d) # Normalize the 1D hough
-    c = -np.sum(h_1d * np.log2(h_1d + 1e-6))
+    c = -np.sum(h_1d * np.log2(h_1d + EPS)) # Add epsilon to avoid log(0)
     return c
 
 
-def hough_entropy(edges, n_theta=10):
+def hough_1d(edges, n_theta=10):
+    """
+    Compute the Hough Transform over angles of the edge image.
+
+    Inputs:
+        edges   - edges to compute entropy on, 1 channel
+        n_theta - number of angular bins to use in hough space
+
+    Returns:
+        h_1d    - 1D hough transform, each bin representing angle range
+    """
     # Classic straight-line Hough transform
     theta = np.linspace(-np.pi/2, np.pi/2, n_theta);
     h,theta,d = hough_line(edges, theta=theta)
 
-    plt.imshow(np.log(1+h),
-            extent=[np.rad2deg(theta[-1]), np.rad2deg(theta[0]), d[-1], d[0]],
-            cmap=cm.gray)
-    plt.title('Hough Space')
-    plt.axis('equal')
-    plt.xlabel('Angle')
-    plt.ylabel('Radius')
-    plt.show()
+    if DEBUG:
+        plt.imshow(np.log(1+h),
+                extent=[np.rad2deg(theta[-1]), np.rad2deg(theta[0]), d[-1], d[0]],
+                cmap=cm.gray)
+        plt.title('Hough Space')
+        plt.axis('equal')
+        plt.xlabel('Angle')
+        plt.ylabel('Radius')
+        plt.show()
 
     h_1d = np.sum(h, axis=0)
 
     return h_1d
 
 
-def computeEdgeSaliency(im, sigma=1, edge_ratio=2):
+def compute_edge_saliency(im, sigma=1, edge_ratio=2):
     """
     Compute the edge saliency of the grayscale image `im`.
 
@@ -100,7 +114,7 @@ def main():
     im = cv2.resize(im, WORKING_RES)
     h,w = im.shape
 
-    edge_sal, eigvec = computeEdgeSaliency(im, edge_ratio = EDGEY_RATIO)
+    edge_sal, eigvec = compute_edge_saliency(im, edge_ratio = EDGEY_RATIO)
 
     #plt.imshow(edge_sal.reshape([h,w]))
     #plt.title('Edge Saliency')
@@ -118,16 +132,13 @@ def main():
     #plt.axis('equal')
     #plt.show()
 
-    #orient_hist, edges = np.histogram(orient, bins=N_THETA)
-    #orient_hist /= np.sum(orient_hist)
-
     #theta_bins = np.linspace(-np.pi/2, np.pi/2, N_THETA);
     theta_bins = np.linspace(-np.pi, np.pi, N_THETA+1);
     bin_ids = np.digitize(orient, theta_bins, right=True)
 
     hist = np.zeros(N_THETA)
     for i in range(N_THETA):
-        hist[i] = np.sum((bin_ids == i) * (edge_sal > 0))
+        hist[i] = np.sum((bin_ids == i) * (edge_sal))
         #hist[i] = np.sum(edge_sal[bin_ids == i])
 
 
@@ -139,7 +150,7 @@ def main():
     plt.bar(center, hist, align='center', width=width)
     plt.show()
 
-    #h_1d = hough_entropy(edge_sal, N_THETA)
+    #h_1d = hough_1d(edge_sal, N_THETA)
     h_1d = hist
     energy = cost(h_1d)
     print(energy)
